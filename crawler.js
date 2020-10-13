c017e6160dc2f375fe4a7bb5d6cd775c93689350
Giang Nam <giangnam10a2@gmail.com>
2020-10-14 01:45:44 +0700

    Remove Link In Comment

--- crawler.js
+++ crawler.js
@@ -9,6 +9,8 @@ const element = {
     ".oajrlxb2.bp9cbjyn.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.pq6dq46d.mg4g778l.btwxx1t3.g5gj957u.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.p8fzw8mz.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.f1sip0of.du4w35lb.lzcic4wl.abiwlrkh.gpro0wi8.m9osqain.buofh1pr",
   tag:
     ".oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.oo9gr5id.gpro0wi8.lrazzd5p",
+  link:
+    ".cwj9ozl2.tvmbv18p .oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.py34i1dx.gpro0wi8",
 };
 
 const crawl = async (url) => {
@@ -47,6 +49,11 @@ const crawl = async (url) => {
   // Remove tag
   await page.$$eval(element.tag, (tags) =>
     tags.map((tag) => (tag.innerHTML = ""))
+  );
+
+  // Remove link
+  await page.$$eval(element.link, (links) =>
+    links.map((links) => (link.innerHTML = ""))
   );
 
   // Get Comments And Reply
